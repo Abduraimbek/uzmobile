ae32ce9f732c8a25a610d2ac42a80aa3a4f82162
Abduraimbek <yarkinovabduraimbek@gmail.com>
2021-02-07 17:00:24 +0500

    yangid

--- lib/screens/screen_language_choose/ScreenLanguageChoose.dart
+++ lib/screens/screen_language_choose/ScreenLanguageChoose.dart
@@ -1,48 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:uzmobile/constants/strings.dart';
-import 'package:uzmobile/screens/rounded_button.dart';
 
 class ScreenLanguageChoose extends StatelessWidget {
   static String routeName = '/ScreenLanguageChoose';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 40,
-              ),
-              child: Image.asset(
-                'assets/images/uzmobile.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          RoundedButton(
-            text: AllStrings.tillar[0],
-            verticalPadding: 20,
-            press: () {},
-          ),
-          RoundedButton(
-            text: AllStrings.tillar[1],
-            verticalPadding: 20,
-            press: () {},
-          ),
-          RoundedButton(
-            text: AllStrings.tillar[2],
-            verticalPadding: 20,
-            press: () {},
-          ),
-        ],
+    return Container(
+      child: Center(
+        child: Text('Language Choose'),
       ),
     );
   }
