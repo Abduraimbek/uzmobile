b606e4cfec286c89e523d8a9142c9d525fe9ce3c
Abduraimbek <yarkinovabduraimbek@gmail.com>
2021-02-07 17:00:24 +0500

    aaaaaaaaaaa

---
+++ lib/constants/shared_preferences.dart
@@ -0,0 +1,7 @@
+class SharedPrefHelper {
+  static String shLanguage = 'sharedPrefLanguage';
+
+  static int uzbek = 0;
+  static int russian = 1;
+  static int english = 2;
+}
---
+++ lib/screens/screen_language_choose/ScreenLanguageChoose.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ScreenLanguageChoose extends StatelessWidget {
+  static String routeName = '/ScreenLanguageChoose';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Language Choose'),
+      ),
+    );
+  }
+}
