3b379da85bc48fd7f2cbdf9eb9f4ccf05b90abb4
Abduraimbek <yarkinovabduraimbek@gmail.com>
2021-02-07 16:16:44 +0500

    aaaaaaaaaaa

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,1 +1,10 @@
-const String a = 'z';
+import 'dart:core';
+
+class StringsForLanguageChange {
+  List<String> uzbekdaDavomEttirish = [
+    "O'zbek tilida davom ettirish",
+    "Продолжить на русскому языку",
+  ];
+
+  List<String> boshlash = ["Boshlash", "Начать"];
+}
