bf6686f1512dfdde176fc3d8595356ac8e379a12
Abduraimbek <yarkinovabduraimbek@gmail.com>
2021-02-08 10:12:04 +0500

    yangid

--- lib/screens/screen_language_choose/ScreenLanguageChoose.dart
+++ lib/screens/screen_language_choose/ScreenLanguageChoose.dart
@@ -19,7 +19,8 @@ class ScreenLanguageChoose extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
-        )
+        ),
+        SizedBox(),
       ],
     );
   }
