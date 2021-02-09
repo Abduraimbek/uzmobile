e5c10109f016029530db94ddee0e6c1d9dbf6304
Abduraimbek <yarkinovabduraimbek@gmail.com>
2021-02-09 20:42:11 +0500

    new

--- lib/screens/screen_home/screen_home.dart
+++ lib/screens/screen_home/screen_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:uzmobile/constants/constants.dart';
 import 'package:uzmobile/widgets/bottom_bar.dart';
 
 class ScreenHome extends StatefulWidget {
@@ -12,25 +13,28 @@ class ScreenHome extends StatefulWidget {
 class _ScreenHomeState extends State<ScreenHome> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Uzmobile'),
-        actions: [
-          GestureDetector(
-            onTap: () {},
-            child: Icon(FlutterIcons.telegram_mco),
-          ),
-          SizedBox(width: 5),
-        ],
+    return Container(
+      color: kMainBlueColor,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Uzmobile'),
+          actions: [
+            GestureDetector(
+              onTap: () {},
+              child: Icon(FlutterIcons.telegram_mco),
+            ),
+            SizedBox(width: 5),
+          ],
+        ),
+        drawer: Drawer(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Color(0xFFF17532),
+          child: Icon(FlutterIcons.md_home_ion),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomBar(),
       ),
-      drawer: Drawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Color(0xFFF17532),
-        child: Icon(FlutterIcons.md_home_ion),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomBar(),
     );
   }
 }
